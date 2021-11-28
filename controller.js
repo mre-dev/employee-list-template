7a9b649110bfa092df93523e8bef28464af85944
mre-dev <mre.dev01@gmail.com>
2021-11-28 14:33:25 +0330

    Update createBox function

--- controller.js
+++ controller.js
@@ -4,7 +4,7 @@ function createBox({firstName, lastName, emial, avatar}) {
     <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 mt-4">
         <div class="box rounded text-center d-flex flex-column justify-content-around">
             <div class="p-3">
-                <img class="rounded-3 w-50" src="${avatar}">
+                <img class="rounded-3 w-50 h-100" src="${avatar}">
             </div>
             <div class="info-box d-flex flex-column justify-content-around">
                 <span class="d-block info-name">${firstName}</span>
